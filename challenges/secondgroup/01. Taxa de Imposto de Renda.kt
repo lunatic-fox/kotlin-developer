b9437adf842f82fc2a016116ad50324f6303d573
Josélio Júnior <76992016+joseliojunior@users.noreply.github.com>
2021-04-06 21:37:18 -0300

    Update 01. Taxa de Imposto de Renda.kt

--- challenges/secondgroup/01. Taxa de Imposto de Renda.kt
+++ challenges/secondgroup/01. Taxa de Imposto de Renda.kt
@@ -2,14 +2,12 @@ package challenges.secondgroup
 
 fun main() {
     fun Float.format(digits: Int) = "%.${digits}f".format(this).replace(',','.')
-    val r = readLine()!!.toFloat()
-    var i = 0f
 
-    when{
-        r <= 2000f -> i = 0f
-        r in 2000.01f..3000f -> i = (r - 2000f ) * 0.08f
-        r in 3000.01f..4500f -> i = 80f + ( r - 3000f ) * 0.18f
-        r > 4500f -> i = 350f + ( r - 4500f ) * 0.28f
+    val i = when (val r = readLine()!!.toFloat()) {
+        in 0f..2000f -> 0f
+        in 2000.01f..3000f -> (r - 2000f ) * 0.08f
+        in 3000.01f..4500f -> 80f + ( r - 3000f ) * 0.18f
+        else -> 350f + ( r - 4500f ) * 0.28f
     }
 
     if (i == 0f) println("Isento") else println("R$ ${i.format(2)}")
