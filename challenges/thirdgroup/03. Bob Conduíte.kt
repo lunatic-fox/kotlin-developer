4b9201604c6ac049b1644c42ce3411acc3ee29b9
Josélio Júnior <76992016+joseliojunior@users.noreply.github.com>
2021-04-08 05:46:15 -0300

    Update 03. Bob Conduíte.kt

--- challenges/thirdgroup/03. Bob Conduíte.kt
+++ challenges/thirdgroup/03. Bob Conduíte.kt
@@ -1,14 +1,8 @@
 package challenges.thirdgroup
 
 fun main() {
-    val t = readLine()!!.toInt()
-    val p = """\s""".toRegex()
-
-    for (i in 1..t) {
-        val rS = readLine()!!
-        val r = rS.split(p)
-        val r1 = r[0].toInt()
-        val r2 = r[1].toInt()
-        println(r1+r2)
+    for (i in 1..readLine()!!.toInt()) {
+        val r = readLine()!!.split("""\s""".toRegex())
+        println(r[0].toInt() + r[1].toInt())
     }
 }
