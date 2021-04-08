25dba4d64dd9ebdeac2201cdce265374c286accb
Josélio Júnior <76992016+joseliojunior@users.noreply.github.com>
2021-04-08 04:05:37 -0300

    Update 04. Corrida das Tartarugas.kt

--- challenges/secondgroup/04. Corrida das Tartarugas.kt
+++ challenges/secondgroup/04. Corrida das Tartarugas.kt
@@ -1,16 +1,16 @@
 package challenges.secondgroup
 
-fun main(){
+fun main() {
     while (true) {
         try {
             val l = readLine()!!.toInt()
             val v = readLine()!!.split(" ").map{it.toInt()}.sorted()
 
-            when{
-                v.last() < 10 -> println(1)
-                v.last() >= 20 -> println(3)
-                else -> println(2)
+            when(v.last()) {
+                in 1..9 -> println(1)
+                in 10..19 -> println(2)
+                else -> println(3)
             }
-        } catch (e : Exception){ break }
+        } catch (e : Exception) { break }
     }
 }
