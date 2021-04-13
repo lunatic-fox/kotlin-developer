5e9ae9a144973d01a8ecb48c6a970c8f2e449c63
Josélio Júnior <76992016+joseliojunior@users.noreply.github.com>
2021-04-13 20:46:59 -0300

    Update 03. Primo Rápido.kt

--- challenges/secondgroup/03. Primo Rápido.kt
+++ challenges/secondgroup/03. Primo Rápido.kt
@@ -3,15 +3,15 @@ import kotlin.math.sqrt
 fun main() {
     val n = readLine()!!.toInt()
     for (i in 1..n) {
-        val x = readLine()!!.toLong()
+        val x = readLine()!!.toDouble()
         println(isPrime(x))
     }
 }
-fun isPrime(n: Long): String{
-    if(( n > 2L && n % 2L == 0L ) || n == 1L ) return "Not Prime"
+fun isPrime(n: Double): String{
+    if(( n > 2.0 && n % 2.0 == 0.0 ) || n == 1.0 ) return "Not Prime"
 
-    for(i in 3L..sqrt(n.toDouble()).toLong()){
-        if( n % i == 0L) return "Not Prime"
+    for(i in 3..sqrt(n).toInt()) {
+        if( n % i == 0.0) return "Not Prime"
     }
     return "Prime"
 }
