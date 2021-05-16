a415a69f82a6dcc0e0d8ae82b3f302e42e5a2de4
joseliojunior <joseliojunior@users.noreply.github.com>
2021-05-16 15:30:17 -0300

    update

--- challenges/thirdgroup/02. Conversão Simples de Base.kt
+++ challenges/thirdgroup/02. Conversão Simples de Base.kt
@@ -4,8 +4,8 @@ fun main() {
     while (true){
         val input = readLine() ?: "-1"
 
-        if (input.matches("""0x.*""".toRegex())) {
-            println( Integer.parseInt(input.removePrefix("0x"), 16) )
+        if (input.matches("0x.*".toRegex())) {
+            println(input.removePrefix("0x").toInt(16))
         } else if (input.toInt() >= 0) {
             println( "0x${Integer.toHexString(input.toInt()).toUpperCase()}" )
         } else { break }
